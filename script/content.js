47f4d2ae43279592b15663f520a4800b16e5991c
Darren Zhu <joodongri@gmail.com>
2019-08-31 10:54:58 -0700

    refactored color change function

--- script/content.js
+++ script/content.js
@@ -1,38 +1,17 @@
 chrome.runtime.onMessage.addListener(function(message) {
-  function colorGen() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-  
-    return "rgb(" + r + "," + g + "," + b + ")";
-  }
 
   if (message === 'secret') {
     $('body').css('background', 'url(https://avatars3.githubusercontent.com/u/6164474?s=460&v=4)');
   }
   if (message === 'night') {
     $('*').css('background-color', '#000');
-    $('h1').css('color', 'grey');
-    $('h2').css('color', 'grey');
-    $('h3').css('color', 'grey');
-    $('h4').css('color', 'grey');
-    $('h5').css('color', 'grey');
-    $('h6').css('color', 'grey');
-    $('p').css('color', 'grey');
-    $('span').css('color', 'grey');
+    changeTextColor('grey');
     $('input').css('color', 'grey');
     $('a').css('color', 'gold');
   }
   if (message === 'day') {
     $('*').css('background-color', '#fff');
-    $('h1').css('color', 'grey');
-    $('h2').css('color', 'grey');
-    $('h3').css('color', 'grey');
-    $('h4').css('color', 'grey');
-    $('h5').css('color', 'grey');
-    $('h6').css('color', 'grey');
-    $('p').css('color', 'grey');
-    $('span').css('color', 'grey');
+    changeTextColor('black');
     $('a').css('color', 'blue');
   }
   if (message === 'disappear') {
@@ -56,6 +35,32 @@ chrome.runtime.onMessage.addListener(function(message) {
     }, 30);
   }
   if (message === 'random') {
+    changeToRandomColor();
+  }
+  if (message === 'party') {
+    let rotation = 0;
+
+    setInterval(function(){
+      changeToRandomColor();
+      $('body').css('transform', `rotate(${rotation += 10}deg)`);
+    }, 30);
+  }
+  if (message === 'reset') {
+    location.reload();
+  }
+
+
+  // random color generation
+  function colorGen() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+  
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+
+  // changes all the components in the webpage to random colors
+  function changeToRandomColor() {
     $('*').css('background-color', colorGen());
     $('div').css('background-color', colorGen());
     $('h1').css('color', colorGen());
@@ -68,26 +73,16 @@ chrome.runtime.onMessage.addListener(function(message) {
     $('span').css('color', colorGen());
     $('a').css('color', colorGen());
   }
-  if (message === 'party') {
-    let rotation = 0;
 
-    setInterval(function(){
-      $('*').css('background-color', colorGen());
-      $('div').css('background-color', colorGen());
-      $('h1').css('color', colorGen());
-      $('h2').css('color', colorGen());
-      $('h3').css('color', colorGen());
-      $('h4').css('color', colorGen());
-      $('h5').css('color', colorGen());
-      $('h6').css('color', colorGen());
-      $('p').css('color', colorGen());
-      $('span').css('color', colorGen());
-      $('a').css('color', colorGen());
-      $('*').css('font-family', "chalkduster");
-      $('body').css('transform', `rotate(${rotation += 10}deg)`);
-    }, 30);
-  }
-  if (message === 'reset') {
-    location.reload();
+  // change all the texts to the passed color
+  function changeTextColor(color) {
+    $('h1').css('color', color);
+    $('h2').css('color', color);
+    $('h3').css('color', color);
+    $('h4').css('color', color);
+    $('h5').css('color', color);
+    $('h6').css('color', color);
+    $('p').css('color', color);
+    $('span').css('color', color);
   }
 });
